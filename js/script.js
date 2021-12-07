8704405079a90e569dd21903d548c5a5be01c415
aquariusboy91 <luca.pellegrino91@yahoo.it>
2021-12-07 13:52:39 +0100

    rev numbers

--- js/script.js
+++ js/script.js
@@ -20,19 +20,47 @@
 // Le validazioni e i controlli possiamo farli
 // in un secondo momento.
 
+/* Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. :bomba:
+I numeri nella lista delle bombe non possono essere duplicati.
+In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati 
+- abbiamo calpestato una bomba - 
+la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l'utente 
+può continuare a cliccare sulle altre celle. La partita termina quando il 
+giocatore clicca su una bomba o raggiunge il numero massimo possibile di 
+numeri consentiti. Al termine della partita il software deve comunicare 
+il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una b.
+ */
 //richiamo il container playfield
 
 let playfield = document.querySelector('.playfield');
 
-//creo un array di numeri random da 1 a 100
+//creo un array di numeri casuali da 1 a 16 (bombe) senza duplicati
+let bomb = [];
+while(bomb.length < 16){
+    let r = Math.floor(Math.random() * 100) + 1;
+    if(bomb.indexOf(r) === -1) bomb.push(r);
+}
+
+console.log(bomb);
+
+
+//creo un array di numeri in sequenza da 1 a 100 con 16 bombe
 let array_100= [];
 i = 0; 
+let numero_rand = 0;
 while (i < 100) {
-    let numero_rand = Math.floor(Math.random() * 100);
+    if(numero_rand == bomb[i]) {
+        array_100.push[bomb[i]];
+    } else if (numero_rand != bomb[i]){
+     numero_rand += 1;
      array_100.push(numero_rand);
      i++;
 }
+}
+
 console.log (array_100);
+
+
 
 //creo un array di numeri random da 1 a 81
 let array_81= [];
